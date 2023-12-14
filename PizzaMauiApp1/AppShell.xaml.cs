5de0c5b9c660174bf6bb9802fc25dceb9c88c1a9
Brice Grenard <brice.grenard@gmail.com>
2023-12-14 09:58:17 +0100

    Initial commit

---
+++ PizzaMauiApp1/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PizzaMauiApp1;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ PizzaMauiApp1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PizzaMauiApp1;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
